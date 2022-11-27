1927abfcd2b43f9c6d7d73daad2bad4606bcab05
Mohamedmorgan26 <mohamedmorgan0100@gmail.com>
2022-11-27 14:54:45 +0200

    Add project files.

---
+++ apple web/apple web/apple.js
@@ -0,0 +1,8 @@
+﻿let img = document.querySelector('.img');
+let container = document.querySelector('.container');
+function phones(phone) {
+    img.src = phone;;
+}
+function colors(color) {
+    container.style.background = color;
+}
